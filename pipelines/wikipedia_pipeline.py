c4293cc97e0d66f8fb7198e969fffb48bb3b09b3
Nando Jr <nandodevs@users.noreply.github.com>
2024-08-02 10:09:41 -0300

    Update code for data pipeline

--- pipelines/wikipedia_pipeline.py
+++ pipelines/wikipedia_pipeline.py
@@ -1,7 +1,12 @@
+import geopy
+from geopy.geocoders import Nominatim
+from datetime import datetime
 import requests
 from lxml import html
 import json
 import pandas as pd
+
+NO_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/No-image-available.png/480px-No-image-available.png'
 
 def get_wikipedia_page(url):
     print("Getting wikipedia page...", url)
@@ -65,13 +70,55 @@ def extract_wikipedia_data(**kwargs):
                 'home_team': clean_text(tds[6].text_content()),
             }
             data.append(values)
-        data_df = pd.DataFrame(data)
-        data_df.to_csv("data/output.csv", index=False)
-        return data
+
+        json_rows = json.dumps(data)    
+        kwargs['ti'].xcom_push(key='rows', value=json_rows)
+
+    # Code OK in Here  
+        # data_df = pd.DataFrame(data)
+        # data_df.to_csv("data/output.csv", index=False)
+        return "OK"
+
+def get_lat_long(country, city):
+    geolocator = Nominatim(user_agent='geoapiExercises')
+    location = geolocator.geocode(f'{city}, {country}')
+
+    if location:
+        return location.latitude, location.longitude
+
+    return None
+
+def transform_wikipedia_data(**kwargs):
+    data = kwargs['ti'].xcom_pull(key='rows', task_ids='extract_data_from_wikipedia')
+
+    data = json.loads(data)
+
+    stadiums_df = pd.DataFrame(data)
+    stadiums_df['location'] = stadiums_df.apply(lambda x: geopy(x['country'], x['stadium']), axis=1)
+    #Function for no Image
+    stadiums_df['images'] = stadiums_df['images'].apply(lambda x: x if x not in ['NO_IMAGE', '', None] else NO_IMAGE) 
+    stadiums_df['capacity'] = stadiums_df['capacity'].astype(int)
+
+    # handle the duplicates
+    duplicates = stadiums_df[stadiums_df.duplicated(['location'])]
+    duplicates['location'] = duplicates.apply(lambda x: get_lat_long(x['country'], x['city']), axis=1)
+    stadiums_df.update(duplicates)
+
+    # push to xcom
+    kwargs['ti'].xcom_push(key='rows', value=stadiums_df.to_json())
+
+    return "OK"
+
+def write_wikipedia_data(**kwargs):
+    data = kwargs['ti'].xcom_pull(key='rows', task_ids='transform_wikipedia_data')
+    data = json.loads(data)
+    data = pd.DataFrame(data)
+
+    file_name = ('stadium_cleaned_' + str(datetime.now().date())
+                 + "_" + str(datetime.now().time()).replace(":", ",") + '.csv')
+
+    data.to_csv('data/' + file_name, index=False)
 
 
-        # json_rows = json.dumps(data)
-        # kwargs['ti'].xcom_push(key='rows', value=json_rows)
 
-    #return "OK"
 
---
+++ test.py
@@ -0,0 +1,4 @@
+import secrets
+
+secret_key = secrets.token_urlsafe(32)
+print(secret_key)
